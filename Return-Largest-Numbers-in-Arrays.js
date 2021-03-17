febab3f192ee0c06558e1bfb5ce544e68d275b64
sharonv153 <39500544+sharonv153@users.noreply.github.com>
2021-03-16 21:58:50 -0700

    Update Return-Largest-Numbers-in-Arrays.js

--- Return-Largest-Numbers-in-Arrays.js
+++ Return-Largest-Numbers-in-Arrays.js
@@ -7,22 +7,31 @@ Remember, you can iterate through an array with a simple for loop, and access ea
 */
 
 
+const largestOfFour = arr => {
+  let numA = [];
+  let largest = 0;
 
-function largestOfFour(arr) {
-let maxNum = 0
-let numArr = [];
-
-for (let i = 0; i < arr.length; i++){
-  maxNum = arr[i][0];
-  for (let h = 0; h < arr[i].length; h++){
-    if (maxNum < arr[i][h]){
-      numArr = arr[i][h]
+  //create loop to iterate through main array
+  for(let i = 0; i < arr.length; i++){
+    //set 'largest' to the first value in inner array to compare with the rest
+    largest = arr[i][0];
+    //create a loop to iterate through inner arrays
+    for (let e = 1; e < arr[i].length; e++){
+      //build a conditional statement to get highest number 'numA' by comparing 'largest' variable to current iteration
+      if (largest < arr[i][e]){
+        largest = arr[i][e]
+      }
     }
+    /* We set the value of 'largest' into position [i] inside the 'numA' array.
+     This will basically only happen when IF is true otherwise the process has nothing to add. */
+    numA[i] = largest;
   }
-  return numArr[i] = maxNum 
-}
+  // Loops are completed and the array is full. Now return the results to the console.
+  return numA
 }
 
+//test returns [5, 27, 39, 1001]
 console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
 
-console.log(largestOfFour([[13, 27, 18, 26], [4, 5, 1, 3], [32, 35, 37, 39], [1000, 1001, 857, 1]]))
+//test returns [9, 35, 97, 1000000]
+console.log(largestOfFour([[4, 9, 1, 3], [13, 35, 18, 26], [32, 35, 97, 39], [1000000, 1001, 857, 1]]))
