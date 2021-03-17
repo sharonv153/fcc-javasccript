ec9cfb3ab5e78d9c0b354cb50a6a0e9931e2c0b3
sharonv153 <39500544+sharonv153@users.noreply.github.com>
2021-03-16 22:46:11 -0700

    Update Return-Largest-Numbers-in-Arrays.js

--- Return-Largest-Numbers-in-Arrays.js
+++ Return-Largest-Numbers-in-Arrays.js
@@ -12,15 +12,13 @@ const largestOfFour = arr => {
   let largest = 0;
 
   //create loop to iterate through main array
-  for(let i = 0; i < arr.length; i++){
+  for(let i = 0; i < arr.length; i++) {
     //set 'largest' to the first value in inner array to compare with the rest
     largest = arr[i][0];
     //create a loop to iterate through inner arrays
-    for (let e = 1; e < arr[i].length; e++){
-      //build a conditional statement to get highest number 'numA' by comparing 'largest' variable to current iteration
-      if (largest < arr[i][e]){
-        largest = arr[i][e]
-      }
+    for (let e = 1; e < arr[i].length; e++) {
+      //build an IF statement to get highest number of 'numA' by comparing 'largest' variable to current iteration
+      largest < arr[i][e] ?  largest = arr[i][e] : largest
     }
     /* We set the value of 'largest' into position [i] inside the 'numA' array.
      This will basically only happen when IF is true otherwise the process has nothing to add. */
