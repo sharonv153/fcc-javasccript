68c21b82835b5ae003263e68a3039f7caad7fa3b
sharonv153 <39500544+sharonv153@users.noreply.github.com>
2021-06-23 14:59:12 -0700

    Create IAS-Diff-Two-Arrays.js

---
+++ IAS-Diff-Two-Arrays.js
@@ -0,0 +1,32 @@
+/*
+Diff Two Arrays
+Compare two arrays and return a new array with any items only found in
+one of the two given arrays, but not both. In other words, return the
+symmetric difference of the two arrays.
+
+Note: You can return the array with its elements in any order.
+*/
+//METHOD 1
+function diffArray(arr1, arr2) {
+  var newArr = [];
+  
+  //combine both arrays into one in a separate variable
+  var combined = arr1.concat(arr2);
+
+//iterate through the concat array
+  for (let a = 0; a < combined.length; a++){
+    //check if any numbers of arr1 aren't in combined
+     if(arr1.includes(combined[a], 0) === false){
+       newArr.push(combined[a]);
+       //now check if any numbers if arr2 aren't in combined
+     } else if (arr2.includes(combined[a], 0) === false){
+       newArr.push(combined[a]);
+     }
+  }
+  
+  return newArr;
+}
+
+//--------------------------------------------------
+
+//METHOD 2
